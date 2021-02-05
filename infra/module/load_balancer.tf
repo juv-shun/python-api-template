c65670cce1943d167976648dbd73da146e6abd7d
Shun Fukusumi <shun.fukusumi@gmail.com>
2021-02-05 22:18:23 +0900

    ロードバランサーの設定を改修

--- infra/module/load_balancer.tf
+++ infra/module/load_balancer.tf
@@ -1,37 +1,18 @@
-# resource "aws_alb" "load_balancer" {
-#   name = var.server_name
-#   security_groups = [
-#     var.default_security_group,
-#     aws_security_group.http_sg.id
-#   ]
-#   subnets = [
-#     var.subnet.az1,
-#     var.subnet.az2,
-#   ]
+resource "aws_alb" "load_balancer" {
+  name = var.server_name
+  security_groups = [
+    var.default_security_group,
+    aws_security_group.http_sg.id
+  ]
+  subnets = [
+    var.subnet.az1,
+    var.subnet.az2,
+  ]
 
-#   access_logs {
-#     bucket  = var.access_logs_bucket
-#     prefix  = var.server_name
-#     enabled = true
-#   }
-# }
-
-resource "aws_alb_target_group" "target_group" {
-  name                 = var.server_name
-  port                 = "80"
-  protocol             = "HTTP"
-  target_type          = "ip"
-  vpc_id               = var.vpc_id
-  deregistration_delay = 90
-
-  health_check {
-    healthy_threshold   = "2"
-    unhealthy_threshold = "4"
-    interval            = "30"
-    matcher             = "200"
-    path                = "/heartbeat"
-    protocol            = "HTTP"
-    timeout             = "5"
+  access_logs {
+    bucket  = var.access_logs_bucket
+    prefix  = var.server_name
+    enabled = true
   }
 }
 
@@ -41,27 +22,126 @@ resource "aws_alb_target_group" "target_group" {
 #   protocol          = "HTTPS"
 #   ssl_policy        = "ELBSecurityPolicy-2016-08"
 #   certificate_arn   = var.certificate_arn
-#
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_alb_target_group.target_group.arn
-#   }
-# }
-
-# resource "aws_alb_listener" "listener_80" {
-#   load_balancer_arn = aws_alb.load_balancer.arn
-#   port              = 80
-#   protocol          = "HTTP"
 
 #   default_action {
 #     type             = "forward"
 #     target_group_arn = aws_alb_target_group.target_group.arn
+#   }
 
-#     # type = "redirect"
-#     # redirect {
-#     #   port        = "443"
-#     #   protocol    = "HTTPS"
-#     #   status_code = "HTTP_301"
-#     # }
+#   lifecycle {
+#     ignore_changes = [default_action]
 #   }
 # }
+
+resource "aws_alb_listener" "listener_80" {
+  load_balancer_arn = aws_alb.load_balancer.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.target_group_blue.arn
+
+    # type = "redirect"
+    # redirect {
+    #   port        = "443"
+    #   protocol    = "HTTPS"
+    #   status_code = "HTTP_301"
+    # }
+  }
+
+  lifecycle {
+    ignore_changes = [default_action]
+  }
+}
+
+resource "aws_alb_listener" "listener_8008" {
+  load_balancer_arn = aws_alb.load_balancer.arn
+  port              = 8008
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_alb_target_group.target_group_blue.arn
+    type             = "forward"
+  }
+
+  lifecycle {
+    ignore_changes = [default_action]
+  }
+}
+
+resource "aws_alb_target_group" "target_group_blue" {
+  name                 = "${var.server_name}-target-blue"
+  port                 = 80
+  protocol             = "HTTP"
+  target_type          = "ip"
+  vpc_id               = var.vpc_id
+  deregistration_delay = 90
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 4
+    interval            = 30
+    matcher             = 200
+    path                = "/heartbeat"
+    protocol            = "HTTP"
+    timeout             = 5
+  }
+}
+
+resource "aws_lb_target_group" "target_group_green" {
+  name                 = "${var.server_name}-target-green"
+  port                 = 80
+  protocol             = "HTTP"
+  target_type          = "ip"
+  vpc_id               = var.vpc_id
+  deregistration_delay = 90
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 4
+    interval            = 30
+    matcher             = 200
+    path                = "/heartbeat"
+    protocol            = "HTTP"
+    timeout             = 5
+  }
+}
+
+resource "aws_alb_target_group" "target_group_test_blue" {
+  name                 = "${var.server_name}-target-test-blue"
+  port                 = 80
+  protocol             = "HTTP"
+  target_type          = "ip"
+  vpc_id               = var.vpc_id
+  deregistration_delay = 90
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 4
+    interval            = 30
+    matcher             = 200
+    path                = "/heartbeat"
+    protocol            = "HTTP"
+    timeout             = 5
+  }
+}
+
+resource "aws_lb_target_group" "target_group_test_green" {
+  name                 = "${var.server_name}-target-test-green"
+  port                 = 80
+  protocol             = "HTTP"
+  target_type          = "ip"
+  vpc_id               = var.vpc_id
+  deregistration_delay = 90
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 4
+    interval            = 30
+    matcher             = 200
+    path                = "/heartbeat"
+    protocol            = "HTTP"
+    timeout             = 5
+  }
+}

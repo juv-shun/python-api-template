57cea6b429dbc6958615c1bfb19d5b890bdc53bd
Shun Fukusumi <shun.fukusumi@gmail.com>
2021-02-05 23:15:39 +0900

    ecrのlifecycleを追加

--- infra/module/ecs.tf
+++ infra/module/ecs.tf
@@ -2,6 +2,30 @@ resource "aws_ecr_repository" "ecr" {
   name = var.server_name
 }
 
+resource "aws_ecr_lifecycle_policy" "ecr_lifecycle" {
+  repository = aws_ecr_repository.ecr.name
+
+  policy = <<EOF
+{
+    "rules": [
+        {
+            "rulePriority": 1,
+            "description": "Delete old images",
+            "selection": {
+                "tagStatus": "any",
+                "countType": "imageCountMoreThan",
+                "countNumber": 30
+            },
+            "action": {
+                "type": "expire"
+            }
+        }
+    ]
+}
+EOF
+}
+
+
 resource "aws_ecs_cluster" "cluster" {
   name = var.server_name
 }

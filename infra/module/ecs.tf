6badec682df185d3c607fdbd516979eb6a2c5bf6
Shun Fukusumi <shun.fukusumi@gmail.com>
2021-02-06 07:48:01 +0900

    terraformコードを整理

--- infra/module/ecs.tf
+++ infra/module/ecs.tf
@@ -92,3 +92,31 @@ locals {
     }
   ]
 }
+
+resource "aws_iam_role" "task_exe_role" {
+  name               = "${var.server_name}-task-exe-role"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.ecs_assume_policy.json
+}
+
+resource "aws_iam_role_policy_attachment" "task_exe_role_attachment" {
+  role       = aws_iam_role.task_exe_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+data "aws_iam_policy_document" "ecs_assume_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "task_role" {
+  name               = "${var.server_name}-task-role"
+  path               = "/"
+  assume_role_policy = data.aws_iam_policy_document.ecs_assume_policy.json
+}
--- infra/module/load_balancer.tf
+++ infra/module/load_balancer.tf
@@ -107,3 +107,41 @@ resource "aws_alb_target_group" "target_group_green" {
     timeout             = 5
   }
 }
+
+resource "aws_security_group" "http_sg" {
+  name        = "${var.server_name}-http-sg"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "[${var.server_name}] http"
+  }
+}

830646acbfe8e78d616ee75fbb4682e8da76ffac
Shun Fukusumi <shun.fukusumi@gmail.com>
2021-02-06 17:43:01 +0900

    deployスクリプトを作成

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,30 +1,24 @@
 #!/bin/bash -e
-SCRIPT_DIR=$(cd $(dirname $0); pwd)
-source ${SCRIPT_DIR}/env.sh
 
-ecs-cli compose \
-    -p ${SYSTEM_NAME} \
-    -f ${SCRIPT_DIR}/docker-compose.yml \
-    --ecs-params ${SCRIPT_DIR}/ecs-params.yml \
-    service up \
-        --target-group-arn ${TARGET_GROUP_ARN} \
-        --container-name app \
-        --container-port 80 \
-        --cluster ${CLUSTER_NAME} \
-        --deployment-max-percent 100 \
-        --deployment-min-healthy-percent 50 \
-        --create-log-groups
+# Set environment variables
+source $(cd $(dirname $0); pwd)/env.sh
 
-echo "service up finished."
+# Register Task Definition
+envsubst < taskdef_template.json > taskdef.json
+aws ecs register-task-definition --cli-input-json file://taskdef.json
+export TASK_DEF_ARN=`aws ecs describe-task-definition --task-definition ${APPLICATION_NAME} | jq '.taskDefinition.taskDefinitionArn' -r`
+echo "*** Task Definition Successfully registered. ${TASK_DEF_ARN}"
 
-ecs-cli compose \
-    -p ${SYSTEM_NAME} \
-    -f ${SCRIPT_DIR}/docker-compose.yml \
-    --ecs-params ${SCRIPT_DIR}/ecs-params.yml \
-    service scale \
-        --cluster ${CLUSTER_NAME} \
-        --deployment-max-percent 200 \
-        --deployment-min-healthy-percent 100 \
-    2
+# Deploy with CodeDeploy
+envsubst < appspec_template.yaml > appspec.yaml
+aws deploy create-deployment \
+    --application-name ${APPLICATION_NAME}\
+    --deployment-group-name ${APPLICATION_NAME}\
+    --revision revisionType=AppSpecContent,appSpecContent={content=\'"`cat appspec.yaml`"\'}
+echo "*** CodeDeploy Successfully started."
+aws ecs update-service --cluster ${APPLICATION_NAME} --service ${APPLICATION_NAME} --desired-count ${ECS_DESIRED_COUNT}
+echo "*** Desired count of ECS tasks successfully updated to ${ECS_DESIRED_COUNT}."
 
-echo "service scale finished."
+# Delete temporary file
+rm -f taskdef.json
+rm -f appspec.yaml
---
+++ deploy/env_template.sh
@@ -0,0 +1,16 @@
+export APPLICATION_NAME=
+
+export AWS_ACCOUNT_ID=
+export IMAGE_TAG=
+
+export CPU_SIZE=256
+export MEMORY_SIZE=512
+export TASK_ROLE_ARN=
+export TASK_EXE_ROLE_ARN=
+export ECS_DESIRED_COUNT=
+
+export DB_HOST=
+export DB_USER=
+# export DB_PASSWORD=
+export DB_NAME=
+export APP_LOG_LEVEL=

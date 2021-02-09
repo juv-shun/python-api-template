e83c974aa57b74265c2a8fdbd0e218a4801b1611
Shun Fukusumi <shun.fukusumi@iridge.jp>
2021-02-09 23:29:18 +0900

    deployディレクトリを移動

--- scripts/deploy/build.sh
+++ scripts/deploy/build.sh
@@ -4,5 +4,5 @@ set -e
 source $(cd $(dirname $0); pwd)/env.sh
 
 aws ecr get-login-password | docker login --username AWS --password-stdin https://${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com
-docker build -t ${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/${APPLICATION_NAME}:${IMAGE_TAG} ..
+docker build -t ${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/${APPLICATION_NAME}:${IMAGE_TAG} ../..
 docker push ${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com/${APPLICATION_NAME}:${IMAGE_TAG}
